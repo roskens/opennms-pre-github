5a58490ba4a251f5fa5f70ed233ca7527f564738
Matt Raykowski <mattraykowski@users.sourceforge.net>
2009-10-15 16:01:49 +0000

    Merged revisions 13475-13482 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/trunk
    ........
      r13475 | rangerrick | 2009-05-21 16:42:29 -0500 (Thu, 21 May 2009) | 1 line
      
      working version of the migrator test/implementation
    ........
      r13476 | rangerrick | 2009-05-22 10:20:06 -0500 (Fri, 22 May 2009) | 1 line
      
      set up (and test) stored procedures/triggers
    ........
      r13477 | rangerrick | 2009-05-22 13:15:59 -0500 (Fri, 22 May 2009) | 1 line
      
      implement rollbacks for post-1.6.0 changes
    ........
      r13478 | thedesloge | 2009-05-22 13:16:37 -0500 (Fri, 22 May 2009) | 1 line
      
      Added GpDetector
    ........
      r13479 | rangerrick | 2009-05-22 14:32:36 -0500 (Fri, 22 May 2009) | 1 line
      
      fix the test to run outside of the full checkout
    ........
      r13480 | rangerrick | 2009-05-25 14:48:57 -0500 (Mon, 25 May 2009) | 1 line
      
      prepare for integrating into the installer
    ........
      r13481 | rssntn67 | 2009-05-26 04:18:31 -0500 (Tue, 26 May 2009) | 2 lines
      
      Fixed an issue related to the groupname definition
    ........
      r13482 | rangerrick | 2009-05-26 14:29:44 -0500 (Tue, 26 May 2009) | 1 line
      
      fix for issue editing an existing optional key on a plugin
    ........

--- core/schema/src/test/java/org/opennms/core/schema/MigratorTest.java
+++ core/schema/src/test/java/org/opennms/core/schema/MigratorTest.java
@@ -1,15 +1,23 @@
 package org.opennms.core.schema;
 
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.sql.DataSource;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.netmgt.dao.db.JUnitTemporaryDatabase;
-import org.opennms.netmgt.dao.db.OpenNMSConfigurationExecutionListener;
 import org.opennms.netmgt.dao.db.TemporaryDatabase;
 import org.opennms.netmgt.dao.db.TemporaryDatabaseExecutionListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -23,18 +31,13 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners({
-    OpenNMSConfigurationExecutionListener.class,
     TemporaryDatabaseExecutionListener.class,
     DependencyInjectionTestExecutionListener.class,
     DirtiesContextTestExecutionListener.class,
     TransactionalTestExecutionListener.class
 })
 @ContextConfiguration(locations={
-        "classpath:/META-INF/opennms/applicationContext-dao.xml",
-        "classpath:/META-INF/opennms/applicationContext-daemon.xml",
-        "classpath:/META-INF/opennms/mockEventIpcManager.xml",
-        "classpath:/META-INF/opennms/applicationContext-setupIpLike-enabled.xml",
-        "classpath*:/META-INF/opennms/component-dao.xml"
+        "classpath:/migratorTest.xml"
 })
 @JUnitTemporaryDatabase(populate=false)
 public class MigratorTest {
@@ -42,14 +45,58 @@ public class MigratorTest {
     @Autowired
     DataSource m_dataSource;
 
+    @Autowired
+    ResourceLoader m_resourceLoader;
+
     private Migration m_migration;
-    
+    private String m_changeLog = "changelog.xml";
+
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        m_migration = new Migration();
+        m_migration.setAdminUser(System.getProperty(TemporaryDatabase.ADMIN_USER_PROPERTY, TemporaryDatabase.DEFAULT_ADMIN_USER));
+        m_migration.setAdminPassword(System.getProperty(TemporaryDatabase.ADMIN_PASSWORD_PROPERTY, TemporaryDatabase.DEFAULT_ADMIN_PASSWORD));
+        m_migration.setDatabaseUser(System.getProperty(TemporaryDatabase.ADMIN_USER_PROPERTY, TemporaryDatabase.DEFAULT_ADMIN_USER));
+        m_migration.setChangeLog(m_changeLog);
     }
 
     @Test
-    public void testNothing() {
-        
+    public void testUpdate() throws Exception {
+        Migrator m = new Migrator();
+        m.setDataSource(m_dataSource);
+        m.migrate(m_migration);
+
+        Connection conn = null;
+        try {
+            conn = m_dataSource.getConnection();
+
+            Set<String> tables = new HashSet<String>();
+            ResultSet rs = conn.getMetaData().getTables(null, null, "%", null);
+            while (rs.next()) {
+                tables.add(rs.getString("TABLE_NAME").toLowerCase());
+            }
+            assertTrue("must contain table 'alarms'", tables.contains("alarms"));
+
+            Set<String> procs = new HashSet<String>();
+            rs = conn.getMetaData().getProcedures(null, null, "%");
+            while (rs.next()) {
+                procs.add(rs.getString("PROCEDURE_NAME").toLowerCase());
+            }
+            System.err.println("procs = " + procs);
+            assertTrue("must have stored procedure 'setSnmpInterfaceKeysOnUpdate'", procs.contains("setsnmpinterfacekeysonupdate"));
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+    
+    @Test
+    @Ignore("takes a long time, just did this to make sure 'upgrades' would not bomb")
+    public void testUpdateTwice() throws Exception {
+        Migrator m = new Migrator();
+        m.setDataSource(m_dataSource);
+        m.migrate(m_migration);
+        m.migrate(m_migration);
     }
 }
