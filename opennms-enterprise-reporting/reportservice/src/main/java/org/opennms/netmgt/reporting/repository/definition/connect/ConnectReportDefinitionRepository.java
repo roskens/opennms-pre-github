7943052ea0f05945509890c9deda6583c7fa9747
Markus Neumann <MarkusNeumannMarkus@gmail.com>
2011-11-07 15:46:59 +0100

    Added tests and error handling

--- opennms-enterprise-reporting/reportservice/src/main/java/org/opennms/netmgt/reporting/repository/definition/connect/ConnectReportDefinitionRepository.java
+++ opennms-enterprise-reporting/reportservice/src/main/java/org/opennms/netmgt/reporting/repository/definition/connect/ConnectReportDefinitionRepository.java
@@ -6,12 +6,13 @@ import java.util.Collection;
 
 import org.opennms.core.utils.LogUtils;
 import org.opennms.netmgt.connect.reports.remote.api.RemoteReportDefinitionRepository;
+import org.opennms.netmgt.connect.reports.remote.api.model.ReportDefinitionSDO;
 import org.opennms.netmgt.reporting.repository.definition.ReportDefinition;
 import org.opennms.netmgt.reporting.repository.definition.ReportDefinitionRepository;
 
 /**
  * Provides access to the report-templates from the opennms connect server.
- * It's the client for {@link DefaultRemoteReportDefinitionRepository}
+ * It's the client for @ {link DefaultRemoteReportDefinitionRepository}
  * 
  * @author thargor
  */
@@ -20,11 +21,6 @@ public class ConnectReportDefinitionRepository implements
 
     private ConnectReportRepositoryConfigDao m_connectReportRepositoryConfigDao;
 
-    // TODO thargor: ConnectConfig
-    //private static final String LOGIN = "okay";
-	//private static final String PASSWORD = "okay";
-	//private static final Boolean active = true;
-	
     // TODO thargor: ConnectReportRepositoryConfig
     private static final String URL = "http://localhost/opennms/rest/connect/reports";
     
@@ -32,50 +28,78 @@ public class ConnectReportDefinitionRepository implements
     private RemoteReportDefinitionRepository m_remoteReportRepository;
 
     @SuppressWarnings("unchecked")
-    @Override
     public Collection<ReportDefinition> getAllReportDefinitions() {
         Collection<ReportDefinition> result = new ArrayList<ReportDefinition>();
 
-        if (m_connectReportRepositoryConfigDao != null && Boolean.TRUE.equals(m_connectReportRepositoryConfigDao.getReportingActive())) {
-         
-        	result = ReportDefinitionSDOMapper.fromCollection(m_remoteReportRepository.getAvailableReportDefinitions());
+        if (m_connectReportRepositoryConfigDao != null) {
+            if (Boolean.TRUE.equals(m_connectReportRepositoryConfigDao.getReportingActive())) {
+                if (m_remoteReportRepository != null) {
 
-//TODO Tak: move that code into a remoteReportRepository implementation?       
+                    Collection<ReportDefinitionSDO> sdoResult = new ArrayList<ReportDefinitionSDO>();
+                    Object temp = m_remoteReportRepository.getAvailableReportDefinitions();
+                    if (temp != null) {
+                        sdoResult.addAll((Collection<ReportDefinitionSDO>) temp);
+                    }
+                    result = ReportDefinitionSDOMapper.fromCollection(sdoResult);
+
+//TODO Tak: move that code into a remoteReportRepository implementation?
 //            DefaultApacheHttpClientConfig config = new DefaultApacheHttpClientConfig();
 //            config.getState().setCredentials(null, null, -1, configDao.getUserName(), configDao.getConnectReportPassword());
 //            ApacheHttpClient c = ApacheHttpClient.create(config);
 //            WebResource r = c.resource(URL);
 //            Collection<?> restResult = r.get(Collection.class);
 //            result = ReportDefinitionSDOMapper.fromCollection((Collection<ReportDefinitionSDO>) restResult);
-            
+                } else {
+                    logRemoteReportRepositoryProblem();
+                }
+            } else {
+                logNotActive();
+            }
         } else {
-            logNotActive();
+            logConfigProblem();
         }
+
 
         return result;
     }
 
-    @Override
     public ReportDefinition getReportDefinition(String name) {
         throw new IllegalStateException(
                                         "requesting reports by name is not supported by remote repository!");
     }
 
-    @Override
     public InputStream getReportTemplate(Integer id, String version) {
-        if (m_connectReportRepositoryConfigDao != null && Boolean.TRUE.equals(m_connectReportRepositoryConfigDao.getReportingActive())) {
-            return m_remoteReportRepository.getReportTemplate(id, version);
+        if (m_connectReportRepositoryConfigDao != null) {
+            if (Boolean.TRUE.equals(m_connectReportRepositoryConfigDao.getReportingActive())) {
+                if (m_remoteReportRepository != null) {
+                    return m_remoteReportRepository.getReportTemplate(id, version);
+                } else {
+                    logRemoteReportRepositoryProblem();
+                    return null;
+                }
+            } else {
+                logConfigProblem();
+                return null;
+            }
         } else {
             logNotActive();
             return null;
         }
-
     }
 
-    @Override
     public ReportDefinition getReportDefinition(Integer id) {
-        if (m_connectReportRepositoryConfigDao != null && Boolean.TRUE.equals(m_connectReportRepositoryConfigDao.getReportingActive())) {
+         if (m_connectReportRepositoryConfigDao != null) {
+            if (Boolean.TRUE.equals(m_connectReportRepositoryConfigDao.getReportingActive())) {
+                if (m_remoteReportRepository != null) {
             return ReportDefinitionSDOMapper.fromSDO(m_remoteReportRepository.getReportDefinition(id));
+                     } else {
+                    logRemoteReportRepositoryProblem();
+                    return null;
+                }
+            } else {
+                logConfigProblem();
+                return null;
+            }
         } else {
             logNotActive();
             return null;
@@ -84,6 +108,14 @@ public class ConnectReportDefinitionRepository implements
 
     private void logNotActive() {
         LogUtils.infof(this, "ConnectReportDefinitionRepository not active");
+    }
+
+    private void logConfigProblem() {
+        LogUtils.infof(this, "ConnectReportRepositoryConfig problem. Config is null");
+    }
+
+    private void logRemoteReportRepositoryProblem() {
+        LogUtils.infof(this, "RemoteReportRepository problem. Repository is null");
     }
 
 	public ConnectReportRepositoryConfigDao getConnectReportRepositoryConfigDao() {
--- opennms-enterprise-reporting/reportservice/src/test/java/org/opennms/netmgt/reporting/repository/definition/connect/ConnectReportDefinitionRepositoryTest.java
+++ opennms-enterprise-reporting/reportservice/src/test/java/org/opennms/netmgt/reporting/repository/definition/connect/ConnectReportDefinitionRepositoryTest.java
@@ -6,6 +6,8 @@ import static org.easymock.EasyMock.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.opennms.netmgt.connect.reports.remote.api.RemoteReportDefinitionRepository;
@@ -20,15 +22,21 @@ public class ConnectReportDefinitionRepositoryTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		mockDao = createMock(ConnectReportRepositoryConfigDao.class);
+        mockDao = createMock(ConnectReportRepositoryConfigDao.class);
 		mockRemoteRepo = createMock(RemoteReportDefinitionRepository.class);
 		repo.setRemoteReportRepository(mockRemoteRepo);
 		repo.setConnectReportRepositoryConfigDao(mockDao);
 	}
 
+    @After
+    public void cleanUp() throws Exception {
+        reset(mockDao);
+        reset(mockRemoteRepo);
+	}
+
 	@Test
 	public final void connectReportingNotActive() {
-		expect(mockDao.getReportingActive()).andReturn(Boolean.FALSE);
+    	expect(mockDao.getReportingActive()).andReturn(Boolean.FALSE);
 		replay(mockDao);
 		List<ReportDefinition> expList = new ArrayList<ReportDefinition>();
 		List<ReportDefinition> resultList = (List<ReportDefinition>) repo.getAllReportDefinitions();
@@ -36,27 +44,69 @@ public class ConnectReportDefinitionRepositoryTest {
 		assertEquals(expList, resultList);
 		verify(mockDao);
 	}
-	
+
+    @Test
+	public final void connectReportingActiveIsNull() {
+    	expect(mockDao.getReportingActive()).andReturn(null);
+		replay(mockDao);
+		List<ReportDefinition> expList = new ArrayList<ReportDefinition>();
+		List<ReportDefinition> resultList = (List<ReportDefinition>) repo.getAllReportDefinitions();
+		assertEquals(0, resultList.size());
+		assertEquals(expList, resultList);
+		verify(mockDao);
+	}
+
 	@Test
 	public final void connectReportingActive() {
-		
 		List<ReportDefinitionSDO> mockList = new ArrayList<ReportDefinitionSDO>();
 		ReportDefinitionSDO repDef1 = new ReportDefinitionSDO();
 		repDef1.setId(1);
 		repDef1.setName("RemoteReport");
 		repDef1.setDescription("First Mocked Remote Report");
 		mockList.add(repDef1);
-		
-		reset(mockDao);
+
 		expect(mockDao.getReportingActive()).andReturn(Boolean.TRUE);
 		replay(mockDao);
-		
-		expect(mockRemoteRepo.getAvailableReportDefinitions()).andReturn(mockList);
+
+        expect(mockRemoteRepo.getAvailableReportDefinitions()).andReturn(mockList);
 		replay(mockRemoteRepo);
 		
 		List<ReportDefinition> resultList = (List<ReportDefinition>) repo.getAllReportDefinitions();
 		assertEquals(1, resultList.size());
 		assertEquals("RemoteReport", resultList.get(0).getName());
 		verify(mockDao);
+        verify(mockRemoteRepo);
+	}
+
+    @Test
+	public final void connectReportingActiveRemoteRepoReturnsNull() {
+		reset(mockDao);
+		expect(mockDao.getReportingActive()).andReturn(Boolean.TRUE);
+		replay(mockDao);
+
+		expect(mockRemoteRepo.getAvailableReportDefinitions()).andReturn(null);
+		replay(mockRemoteRepo);
+
+		List<ReportDefinition> resultList = (List<ReportDefinition>) repo.getAllReportDefinitions();
+		assertEquals(0, resultList.size());
+		verify(mockDao);
+        verify(mockRemoteRepo);
+	}
+
+    @Test
+	public final void connectReportingActiveRemoteRepoIsNull() {
+		expect(mockDao.getReportingActive()).andReturn(Boolean.TRUE);
+		replay(mockDao);
+        repo.setRemoteReportRepository(null);
+		List<ReportDefinition> resultList = (List<ReportDefinition>) repo.getAllReportDefinitions();
+		assertEquals(0, resultList.size());
+		verify(mockDao);
+	}
+
+    @Test
+	public final void connectReportingConfigIsNull() {
+        repo.setConnectReportRepositoryConfigDao(null);
+		List<ReportDefinition> resultList = (List<ReportDefinition>) repo.getAllReportDefinitions();
+		assertEquals(0, resultList.size());
 	}
 }
