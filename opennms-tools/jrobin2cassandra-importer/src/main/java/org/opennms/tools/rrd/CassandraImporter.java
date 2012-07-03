dfa00fa0de7a58e0f3b06d85dab8132779c33559
Ronald Roskens <roskens@users.sourceforge.net>
2012-07-02 20:26:22 -0500

    initial commit

---
+++ opennms-tools/jrobin2cassandra-importer/src/main/java/org/opennms/tools/rrd/CassandraImporter.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2006-2011 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+
+package org.opennms.tools.rrd;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.opennms.netmgt.rrd.RrdDataSource;
+import org.opennms.netmgt.rrd.RrdStrategy;
+import org.opennms.netmgt.rrd.RrdUtils;
+
+public class CassandraImporter {
+
+    static RrdStrategy<Object,Object> rrd = null;
+
+    public static void main(final String[] args) throws Exception {
+
+        String strategy = System.getProperty("org.opennms.rrd.strategyClass");
+        if (strategy == null) {
+            System.setProperty("org.opennms.rrd.strategyClass", "org.opennms.netmgt.rrd.jrobin.JRobinRrdStrategy");
+        }
+
+        rrdInitialize();
+
+    }
+
+    private static void rrdInitialize() throws Exception {
+	rrd = RrdUtils.getStrategy();
+    }
+
+}
