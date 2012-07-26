c53e3d84015b15cba396501c4561d1f29aa28055
Ronny Trommer <ronny@opennms.org>
2012-07-26 17:46:04 +0200

    Javadoc added to GenericURLStreamHandler

--- opennms-util/src/main/java/org/opennms/core/utils/url/GenericURLStreamHandler.java
+++ opennms-util/src/main/java/org/opennms/core/utils/url/GenericURLStreamHandler.java
@@ -1,3 +1,31 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2009-2011 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
 package org.opennms.core.utils.url;
 
 import java.io.IOException;
@@ -7,6 +35,12 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLStreamHandler;
 
+/**
+ * Generate an URL stream handler
+ *
+ * @author <a href="mailto:christian.pape@informatik.hs-fulda.de">Christian Pape</a>
+ * @author <a href="mailto:ronny@opennms.org">Ronny Trommer</a>
+ */
 public class GenericURLStreamHandler extends URLStreamHandler {
     Class<URLConnection> urlConnectionClass;
 
@@ -14,6 +48,9 @@ public class GenericURLStreamHandler extends URLStreamHandler {
         this.urlConnectionClass = urlConnectionClass;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected URLConnection openConnection(URL u) throws IOException {
         URLConnection urlConnection = null;
@@ -39,6 +76,9 @@ public class GenericURLStreamHandler extends URLStreamHandler {
         return urlConnection;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void parseURL(URL u, String spec, int start, int limit) {
         super.parseURL(u, spec, start, limit);
