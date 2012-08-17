7acadf2c53e4ed46d9018debf8cd3d3e78ee7636
Benjamin Reed <rangerrick@befunk.com>
2012-08-17 14:22:50 -0400

    almost working

---
+++ container/servlet/src/main/java/org/opennms/container/web/ProxyFilter.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.opennms.container.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.framework.BundleContext;
+
+public final class ProxyFilter implements Filter {
+    private DispatcherTracker m_dispatcherTracker;
+    private FilterConfig m_filterConfig;
+
+    @Override
+    public void destroy() {
+        this.m_dispatcherTracker.close();
+    }
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        m_filterConfig = filterConfig;
+        filterConfig.getServletContext().log("initializing filter config " + filterConfig);
+        try {
+            this.m_dispatcherTracker = new DispatcherTracker(getBundleContext(filterConfig.getServletContext()), null, filterConfig);
+        } catch (final ServletException e) {
+            throw e;
+        } catch (final Exception e) {
+            throw new ServletException("Unable to create dispatcher m_dispatcherTracker.", e);
+        }
+        filterConfig.getServletContext().log("opening dispatcher m_dispatcherTracker");
+        this.m_dispatcherTracker.open();
+        filterConfig.getServletContext().log("finished initializing");
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        m_filterConfig.getServletContext().log("doFilter() called.");
+        m_filterConfig.getServletContext().log("request: " + request);
+        m_filterConfig.getServletContext().log("response: " + response);
+        m_filterConfig.getServletContext().log("filter chain: " + chain);
+
+        final Filter dispatcher = this.m_dispatcherTracker.getDispatcher();
+        m_filterConfig.getServletContext().log("dispatcher: " + dispatcher);
+        if (dispatcher != null) {
+            dispatcher.doFilter(request, response, chain);
+        } else {
+            chain.doFilter(request, response);
+        }
+        m_filterConfig.getServletContext().log("doFilter() finished.");
+    }
+
+    private BundleContext getBundleContext(final ServletContext servletContext) throws ServletException {
+        final Object context = servletContext.getAttribute(BundleContext.class.getName());
+        if (context instanceof BundleContext) {
+            return (BundleContext)context;
+        }
+
+        throw new ServletException("Bundle context attribute [" + BundleContext.class.getName() + "] not set in servlet context");
+    }
+}
