9d3c99dc95e0e880237d5f3a3afdcf5ec4c5d61f
Ronald Roskens <roskens@users.sourceforge.net>
2012-09-01 12:32:17 -0500

    Format Persister.java

--- opennms-rrd/opennms-rrd-cassandra/src/main/java/org/opennms/netmgt/rrd/cassandra/Persister.java
+++ opennms-rrd/opennms-rrd-cassandra/src/main/java/org/opennms/netmgt/rrd/cassandra/Persister.java
@@ -16,26 +16,34 @@ import me.prettyprint.hector.api.mutation.Mutator;
 import org.opennms.core.utils.LogUtils;
 
 class Persister implements Runnable {
-	Keyspace m_keyspace;
-	String m_dpColumnFamily;
-	int m_ttl;
-	BlockingQueue<Datapoint> m_queue = new LinkedBlockingQueue<Datapoint>();
-	int m_maxBatchSize = 100000;
-	int m_naglesDelay = 0;
-	Thread m_thread;
-	AtomicBoolean m_finish = new AtomicBoolean(false);
-	CountDownLatch m_finishLatch = new CountDownLatch(1);
+    Keyspace m_keyspace;
 
-	private static final StringSerializer s_ss = StringSerializer.get();
+    String m_dpColumnFamily;
 
-	Persister(Keyspace keyspace, String dpColumnFamily, int ttl) {
-		m_keyspace = keyspace;
-		m_dpColumnFamily = dpColumnFamily;
-		m_ttl = ttl;
-		m_thread = new Thread(this, "Cassandra-Persister");
-		m_thread.setDaemon(true);
-		m_thread.start();
-	}
+    int m_ttl;
+
+    BlockingQueue<Datapoint> m_queue = new LinkedBlockingQueue<Datapoint>();
+
+    int m_maxBatchSize = 100000;
+
+    int m_naglesDelay = 0;
+
+    Thread m_thread;
+
+    AtomicBoolean m_finish = new AtomicBoolean(false);
+
+    CountDownLatch m_finishLatch = new CountDownLatch(1);
+
+    private static final StringSerializer s_ss = StringSerializer.get();
+
+    Persister(Keyspace keyspace, String dpColumnFamily, int ttl) {
+        m_keyspace = keyspace;
+        m_dpColumnFamily = dpColumnFamily;
+        m_ttl = ttl;
+        m_thread = new Thread(this, "Cassandra-Persister");
+        m_thread.setDaemon(true);
+        m_thread.start();
+    }
 
     public List<Datapoint> getDatapoints() throws InterruptedException {
         List<Datapoint> datapoints = m_maxBatchSize > 0 ? new ArrayList<Datapoint>(m_maxBatchSize) : new ArrayList<Datapoint>();
@@ -66,55 +74,56 @@ class Persister implements Runnable {
 
     }
 
-	public void drainTo(List<Datapoint> datapoints) {
-		if (m_maxBatchSize > 0) {
-			m_queue.drainTo(datapoints, m_maxBatchSize - datapoints.size());
-		} else {
-			m_queue.drainTo(datapoints);
-		}
-	}
+    public void drainTo(List<Datapoint> datapoints) {
+        if (m_maxBatchSize > 0) {
+            m_queue.drainTo(datapoints, m_maxBatchSize - datapoints.size());
+        } else {
+            m_queue.drainTo(datapoints);
+        }
+    }
 
-	public boolean hasMoreRoom(List<Datapoint> datapoints) {
-		return m_maxBatchSize <= 0 || datapoints.size() < m_maxBatchSize;
-	}
+    public boolean hasMoreRoom(List<Datapoint> datapoints) {
+        return m_maxBatchSize <= 0 || datapoints.size() < m_maxBatchSize;
+    }
 
-	public void run() {
-		while (true) {
-		try {
-			if (m_finish.get() && m_queue.peek() == null) {
-				m_finishLatch.countDown();
-				return;
-			}
-			List<Datapoint> datapoints = getDatapoints();
-			if (datapoints == null) continue;
+    public void run() {
+        while (true) {
+            try {
+                if (m_finish.get() && m_queue.peek() == null) {
+                    m_finishLatch.countDown();
+                    return;
+                }
+                List<Datapoint> datapoints = getDatapoints();
+                if (datapoints == null)
+                    continue;
 
-			long start = System.currentTimeMillis();
+                long start = System.currentTimeMillis();
 
-			Mutator<String> mutator = HFactory.createMutator(m_keyspace, s_ss);
+                Mutator<String> mutator = HFactory.createMutator(m_keyspace, s_ss);
 
-			for(Datapoint datapoint : datapoints) {
-				datapoint.persist(mutator, m_dpColumnFamily, m_ttl);
-			}
+                for (Datapoint datapoint : datapoints) {
+                    datapoint.persist(mutator, m_dpColumnFamily, m_ttl);
+                }
 
-			mutator.execute();
-			long end = System.currentTimeMillis();
+                mutator.execute();
+                long end = System.currentTimeMillis();
 
-			LogUtils.debugf(this, "Wrote %d datapoints in %d ms.", datapoints.size(), end-start);
+                LogUtils.debugf(this, "Wrote %d datapoints in %d ms.", datapoints.size(), end - start);
 
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			LogUtils.errorf(this, e, "failure?");
-		}
-		}
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                LogUtils.errorf(this, e, "failure?");
+            }
+        }
 
-	}
+    }
 
-	public void persist(Datapoint datapoint) {
-		m_queue.offer(datapoint);
-	}
+    public void persist(Datapoint datapoint) {
+        m_queue.offer(datapoint);
+    }
 
-	public void waitForFinish() throws InterruptedException {
-		m_finish.set(true);
-		m_finishLatch.await();
-	}
+    public void waitForFinish() throws InterruptedException {
+        m_finish.set(true);
+        m_finishLatch.await();
+    }
 }
