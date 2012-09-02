43487aa2467d1b10436edf8256c25d865ba3e682
Ronald Roskens <roskens@users.sourceforge.net>
2012-09-01 22:55:02 -0500

    Fix compile

--- opennms-rrd/opennms-rrd-cassandra/src/test/java/org/opennms/netmgt/rrd/cassandra/CassandraTest.java
+++ opennms-rrd/opennms-rrd-cassandra/src/test/java/org/opennms/netmgt/rrd/cassandra/CassandraTest.java
@@ -259,11 +259,9 @@ public class CassandraTest {
 
 	Keyspace keyspace = HFactory.createKeyspace(KEYSPACE, cluster);
 
+        CassandraRrdConnection connection = new CassandraRrdConnection(keyspace, "", COLUMN_FAMILY, 86400);
 
-
-
-
-	Persister persister = new Persister(keyspace, COLUMN_FAMILY, 86400);
+	Persister persister = connection.getPersister();
 
 
 	Timer writeTime = new Timer();
@@ -381,9 +379,10 @@ public class CassandraTest {
 
 	Keyspace keyspace = HFactory.createKeyspace(KEYSPACE, cluster);
 
+        // only let data live for 30 seconds
+        CassandraRrdConnection connection = new CassandraRrdConnection(keyspace, "", COLUMN_FAMILY, 30);
 
-	// only let data live for 30 seconds
-	Persister persister = new Persister(keyspace, COLUMN_FAMILY, 30);
+        Persister persister = connection.getPersister();
 
 	Timer writeTime = new Timer();
 
