500d465c5c6b6cf34c40aa312f11e3f9af3024ac
Ronald Roskens <ronald.roskens@gmail.com>
2013-01-06 22:10:34 -0600

    format

--- opennms-rrd/opennms-rrd-cassandra/src/main/java/org/opennms/netmgt/rrd/cassandra/DataPointColumn.java
+++ opennms-rrd/opennms-rrd-cassandra/src/main/java/org/opennms/netmgt/rrd/cassandra/DataPointColumn.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.opennms.netmgt.rrd.cassandra;
 
 import com.netflix.astyanax.annotations.Component;
@@ -11,8 +7,13 @@ import com.netflix.astyanax.annotations.Component;
  * @author roskens
  */
 public class DataPointColumn {
-    @Component(ordinal=0) String m_dsName;
-    @Component(ordinal=1) Long m_timestamp;
+
+    @Component(ordinal = 0)
+    String m_dsName;
+
+    @Component(ordinal = 1)
+    Long m_timestamp;
+
     public DataPointColumn(final String dsName, final Long timestamp) {
         m_dsName = dsName;
         m_timestamp = timestamp;
