0a13570940fd3b4d717a6b9d441c66877fff880a
Joe Jones <jjones@opennms.org>
2013-07-09 11:46:14 -0400

    updated vaadin geo maps

--- features/vaadin-node-maps/src/main/java/org/opennms/features/vaadin/nodemaps/internal/NodeMapsApplication.java
+++ features/vaadin-node-maps/src/main/java/org/opennms/features/vaadin/nodemaps/internal/NodeMapsApplication.java
@@ -88,7 +88,8 @@ import java.io.InputStream;
         "http://maps.google.com/maps/api/js?sensor=false",
         "http://cdn.leafletjs.com/leaflet-0.5.1/leaflet-src.js",
         "gwt/public/openlayers/OpenLayers.js",
-        "gwt/public/markercluster/leaflet.markercluster.js"
+        "gwt/public/markercluster/leaflet.markercluster-src.js"
+        
 })
 @StyleSheet({
         "gwt/public/markercluster/MarkerCluster.css",
