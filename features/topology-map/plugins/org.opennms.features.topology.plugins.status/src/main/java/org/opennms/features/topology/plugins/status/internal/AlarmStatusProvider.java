767415e1fc960d8541968d52a31a96b2cf8a6aec
Ronald Roskens <ronald.roskens@gmail.com>
2013-07-20 16:26:14 -0500

    fix checkstyle: Add missing javadoc comment

--- features/topology-map/plugins/org.opennms.features.topology.plugins.status/src/main/java/org/opennms/features/topology/plugins/status/internal/AlarmStatusProvider.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.status/src/main/java/org/opennms/features/topology/plugins/status/internal/AlarmStatusProvider.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2012 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2012 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
 package org.opennms.features.topology.plugins.status.internal;
 
 import java.util.ArrayList;
@@ -17,27 +44,52 @@ import org.opennms.netmgt.dao.api.AlarmDao;
 import org.opennms.netmgt.model.OnmsSeverity;
 import org.opennms.netmgt.model.alarm.AlarmSummary;
 
+/**
+ * The Class AlarmStatusProvider.
+ */
 public class AlarmStatusProvider implements StatusProvider {
 
+    /**
+     * The Class AlarmStatus.
+     */
     public class AlarmStatus implements Status {
 
+        /** The m_status id. */
         private int m_statusId;
 
+        /** The m_label. */
         private String m_label;
 
+        /** The m_alarm count. */
         private int m_alarmCount = 0;
 
+        /**
+         * Instantiates a new alarm status.
+         *
+         * @param id
+         *            the id
+         * @param label
+         *            the label
+         * @param count
+         *            the count
+         */
         public AlarmStatus(int id, String label, int count) {
             m_statusId = id;
             m_label = label;
             m_alarmCount = count;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Status#computeStatus()
+         */
         @Override
         public String computeStatus() {
             return m_label.toLowerCase();
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Status#getStatusProperties()
+         */
         @Override
         public Map<String, String> getStatusProperties() {
             Map<String, String> statusMap = new HashMap<String, String>();
@@ -48,26 +100,53 @@ public class AlarmStatusProvider implements StatusProvider {
 
     }
 
+    /** The m_alarm dao. */
     private AlarmDao m_alarmDao;
 
+    /** The m_vertex provider. */
     private VertexProvider m_vertexProvider;
 
+    /**
+     * Gets the vertex provider.
+     *
+     * @return the vertex provider
+     */
     public VertexProvider getVertexProvider() {
         return m_vertexProvider;
     }
 
+    /**
+     * Sets the vertex provider.
+     *
+     * @param vertexProvider
+     *            the new vertex provider
+     */
     public void setVertexProvider(VertexProvider vertexProvider) {
         m_vertexProvider = vertexProvider;
     }
 
+    /**
+     * Gets the alarm dao.
+     *
+     * @return the alarm dao
+     */
     public AlarmDao getAlarmDao() {
         return m_alarmDao;
     }
 
+    /**
+     * Sets the alarm dao.
+     *
+     * @param alarmDao
+     *            the new alarm dao
+     */
     public void setAlarmDao(AlarmDao alarmDao) {
         m_alarmDao = alarmDao;
     }
 
+    /* (non-Javadoc)
+     * @see org.opennms.features.topology.api.topo.StatusProvider#getStatusForVertex(org.opennms.features.topology.api.topo.VertexRef)
+     */
     @Override
     public Status getStatusForVertex(VertexRef vertexRef) {
 
@@ -89,6 +168,9 @@ public class AlarmStatusProvider implements StatusProvider {
 
     }
 
+    /* (non-Javadoc)
+     * @see org.opennms.features.topology.api.topo.StatusProvider#getStatusForVertices(java.util.Collection)
+     */
     @Override
     public Collection<Status> getStatusForVertices(Collection<VertexRef> vertices) {
         Collection<Status> verticesStatus = new ArrayList<Status>();
@@ -98,11 +180,21 @@ public class AlarmStatusProvider implements StatusProvider {
         return verticesStatus;
     }
 
+    /* (non-Javadoc)
+     * @see org.opennms.features.topology.api.topo.StatusProvider#getNamespace()
+     */
     @Override
     public String getNamespace() {
         return "node-alarm-status";
     }
 
+    /**
+     * Gets the alarm status.
+     *
+     * @param nodeIds
+     *            the node ids
+     * @return the alarm status
+     */
     private Status getAlarmStatus(Collection<Integer> nodeIds) {
         List<AlarmSummary> alarmSummaries = m_alarmDao.getNodeAlarmSummaries(nodeIds.toArray(new Integer[nodeIds.size()]));
         if (alarmSummaries != null && alarmSummaries.size() >= 1) {
@@ -112,6 +204,14 @@ public class AlarmStatusProvider implements StatusProvider {
         }
     }
 
+    /**
+     * Adds the children recursively.
+     *
+     * @param groupRef
+     *            the group ref
+     * @param nodeIds
+     *            the node ids
+     */
     private void addChildrenRecursively(VertexRef groupRef, Collection<Integer> nodeIds) {
         List<Vertex> vertices = getVertexProvider().getChildren(groupRef);
         for (Vertex vertex : vertices) {
@@ -123,6 +223,13 @@ public class AlarmStatusProvider implements StatusProvider {
         }
     }
 
+    /**
+     * Checks if is group.
+     *
+     * @param vertexRef
+     *            the vertex ref
+     * @return true, if is group
+     */
     private boolean isGroup(VertexRef vertexRef) {
         if (vertexRef instanceof Vertex) {
             return ((Vertex) vertexRef).isGroup();
@@ -130,10 +237,22 @@ public class AlarmStatusProvider implements StatusProvider {
         return false;
     }
 
+    /**
+     * Creates the indeterminate status.
+     *
+     * @return the status
+     */
     private Status createIndeterminateStatus() {
         return new AlarmStatus(OnmsSeverity.INDETERMINATE.getId(), OnmsSeverity.INDETERMINATE.getLabel(), 0);
     }
 
+    /**
+     * Calculate alarm status.
+     *
+     * @param alarmSummaries
+     *            the alarm summaries
+     * @return the alarm status
+     */
     private AlarmStatus calculateAlarmStatus(List<AlarmSummary> alarmSummaries) {
         Collections.sort(alarmSummaries, new Comparator<AlarmSummary>() {
             @Override
--- features/topology-map/plugins/org.opennms.features.topology.plugins.status/src/test/java/org/opennms/features/topology/plugins/status/internal/AlarmStatusProviderTest.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.status/src/test/java/org/opennms/features/topology/plugins/status/internal/AlarmStatusProviderTest.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2012 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2012 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
 package org.opennms.features.topology.plugins.status.internal;
 
 import static org.junit.Assert.assertTrue;
@@ -19,78 +46,129 @@ import org.opennms.netmgt.model.alarm.AlarmSummary;
 
 import com.vaadin.data.Item;
 
+/**
+ * The Class AlarmStatusProviderTest.
+ */
 public class AlarmStatusProviderTest {
 
+    /**
+     * The Class TestVertex.
+     */
     private class TestVertex extends AbstractRef implements Vertex {
 
+        /**
+         * Instantiates a new test vertex.
+         */
         public TestVertex() {
             super("nodes", "1", null);
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getKey()
+         */
         @Override
         public String getKey() {
             return null;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getItem()
+         */
         @Override
         public Item getItem() {
             return null;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getTooltipText()
+         */
         @Override
         public String getTooltipText() {
             return null;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getIconKey()
+         */
         @Override
         public String getIconKey() {
             return null;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getStyleName()
+         */
         @Override
         public String getStyleName() {
             return null;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#isGroup()
+         */
         @Override
         public boolean isGroup() {
             return false;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#setParent(org.opennms.features.topology.api.topo.VertexRef)
+         */
         @Override
         public void setParent(VertexRef parent) {
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getParent()
+         */
         @Override
         public VertexRef getParent() {
             return null;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getX()
+         */
         @Override
         public Integer getX() {
             return null;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getY()
+         */
         @Override
         public Integer getY() {
             return null;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#isLocked()
+         */
         @Override
         public boolean isLocked() {
             return false;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#isSelected()
+         */
         @Override
         public boolean isSelected() {
             return false;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getIpAddress()
+         */
         @Override
         public String getIpAddress() {
             return null;
         }
 
+        /* (non-Javadoc)
+         * @see org.opennms.features.topology.api.topo.Vertex#getNodeID()
+         */
         @Override
         public Integer getNodeID() {
             return 1;
@@ -98,10 +176,15 @@ public class AlarmStatusProviderTest {
 
     }
 
+    /** The m_alarm dao. */
     private AlarmDao m_alarmDao;
 
+    /** The m_status provider. */
     private AlarmStatusProvider m_statusProvider;
 
+    /**
+     * Sets the up.
+     */
     @Before
     public void setUp() {
         m_alarmDao = EasyMock.createMock(AlarmDao.class);
@@ -109,6 +192,9 @@ public class AlarmStatusProviderTest {
         m_statusProvider.setAlarmDao(m_alarmDao);
     }
 
+    /**
+     * Test get alarm status.
+     */
     @Test
     public void testGetAlarmStatus() {
         Vertex vertex = new TestVertex();
@@ -124,6 +210,11 @@ public class AlarmStatusProviderTest {
         EasyMock.verify(m_alarmDao);
     }
 
+    /**
+     * Creates the normal alarm summary list.
+     *
+     * @return the list
+     */
     private List<AlarmSummary> createNormalAlarmSummaryList() {
         List<AlarmSummary> alarms = new ArrayList<AlarmSummary>();
         alarms.add(new AlarmSummary(1, "node1", new Date(), OnmsSeverity.INDETERMINATE, 1L));
